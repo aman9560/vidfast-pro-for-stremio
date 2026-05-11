f64f3c98e7607b72c82472cf64b626f7a05b9fa1
aman9560 <47471236+aman9560@users.noreply.github.com>
2026-05-12 02:26:12 +0530

    Revise manifest.js for VidFast Pro addon
    Updated the manifest to reflect changes in description, resources, and idPrefixes.

--- manifest.js
+++ manifest.js
@@ -1,27 +1,12 @@
 module.exports = {
-  id: 'org.vidfast.pro',
-  version: '1.0.0',
-  name: 'VidFast Pro',
-  description: 'Stremio addon that exposes VidFast movie and series metadata + streams.',
-  resources: ['catalog', 'meta', 'stream'],
-  types: ['movie', 'series'],
-  idPrefixes: ['vf:', 'tt'],
-  catalogs: [
-    {
-      type: 'movie',
-      id: 'vidfast_movies',
-      name: 'VidFast Movies',
-      extra: [{ name: 'search', isRequired: false }, { name: 'skip' }]
-    },
-    {
-      type: 'series',
-      id: 'vidfast_series',
-      name: 'VidFast TV Shows',
-      extra: [{ name: 'search', isRequired: false }, { name: 'skip' }]
-    }
-  ],
-  behaviorHints: {
-    configurable: false,
-    adult: false
-  }
+    id: 'org.vidfast.pro',
+    version: '1.0.0',
+    name: 'VidFast Pro',
+    description: 'VidFast streams for Stremio',
+
+    resources: ['stream'],
+
+    types: ['movie', 'series'],
+
+    idPrefixes: ['tt']
 }
